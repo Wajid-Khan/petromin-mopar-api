6ae013c6076ddc124f3f83ee537419f628a7a71c
waseem111 <55344039+waseem111@users.noreply.github.com>
2026-03-29 14:21:26 +0530

    images and path added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 const contactRoutes = require("./src/routes/contactRoutes");
 const vehicleRoutes = require("./src/routes/vehicleRoutes");
 const lookupRoutes = require("./src/routes/lookupRoutes");
@@ -14,6 +14,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use("/api/contact", contactRoutes);
 app.use("/api/vehicle", vehicleRoutes);
@@ -26,5 +27,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3006;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Petromin Mopar API running on port 3006');
+  console.log('Petromin Mopar API running on port 3006');
 });
